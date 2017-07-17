0d4329b9dd7532512ce78e3cedb843f6cec7fd52
Milan Chodvadiya <cmilan@pistalix.com>
2017-07-17 12:17:52 +0530

    pub

--- dato.config.js
+++ dato.config.js
@@ -5,16 +5,3 @@ module.exports = (dato, root, i18n) => {
   const content = { hello: "world" }
   root.createDataFile("data/foobar.yml", 'yaml', content)
 }
-
-module.exports = (dato, root, i18n) => {
-  root.createPost("content/post/my-post.md", "yaml", {
-    frontmatter: {
-      title: "My First Post",
-      type: "post",
-      categories: ["random"],
-      weight: 4,
-      date: "2017-07-17",
-    },
-    content: "Lorem **ipsum dolor sit amet**, consectetur adipiscing elit."
-  });
-}
